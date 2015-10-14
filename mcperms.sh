5ddee65ef75205a4235e7e2c0326813d501989df
Stewart Smith <stxyzth@users.noreply.github.com>
2015-10-14 10:09:00 +0100

    Update mcperms.sh

--- mcperms.sh
+++ mcperms.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 filename='chown.txt'
 
-ls -l | awk '{ print $9 }' | awk -F '[^0-9]+' '{OFS=" "; for(i=1; i<=NF; ++i) if ($i != "") print($i)}' > chown.txt
+ls -l | awk '{ print $9 }' | awk -F '[^0-9]+' '{OFS=" "; for(i=1; i<=NF; ++i) if ($i != "") print($i)}' > $filename
 
 sleep 6
 
@@ -9,4 +9,4 @@ while read p; do
     `chown -R mc$p:mc$p server$p/`
 done < $filename
 
-rm chown.txt
+rm $filename
